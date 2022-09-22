8ff3dddb5098887e99db718d2771b788740a8990
s-kella <52741545+s-kella@users.noreply.github.com>
2022-09-22 13:52:36 -0500

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,31 +1,33 @@
 import json
 import os
-from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from livereload import Server
+
+
+def on_reload():
+    rendered_page = template.render(
+        books=books_for_rendering)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
-
 template = env.get_template('template.html')
 
 with open("books.json", "r", encoding="utf8") as books_file:
     books = books_file.read()
 books = json.loads(books)
-
 books_for_rendering = []
 for book in books:
     books_for_rendering.append({'author': book['author'],
-                            'title': book['title'],
-                            'pic' : os.path.join('covers', book['img name'])})
+                                'title': book['title'],
+                                'pic': os.path.join('covers', book['img name'])})
+on_reload()
 
-rendered_page = template.render(
-    authors_n_names=books_for_rendering,
-)
+server = Server()
+server.watch('template.html', on_reload)
+server.serve(root='.')
 
-with open('index.html', 'w', encoding="utf8") as books_file:
-    books_file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
