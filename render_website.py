16acd5e65ecbf5371b97407de6dd56b030b2f773
s-kella <52741545+s-kella@users.noreply.github.com>
2022-09-26 15:13:13 -0500

    split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,21 +1,23 @@
 import json
 import os
-import math
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
 
 def on_reload():
-    rendered_page = template.render(books=books_for_rendering)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for i, page in enumerate(books_for_rendering):
+        books = list(chunked(page, 2))
+        rendered_page = template.render(books=books)
+        page_path = os.path.join('pages', f'index{i}.html')
+        with open(page_path, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
+os.makedirs('pages', exist_ok=True)
 env = Environment(
     loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+    autoescape=select_autoescape(['html', 'xml']))
 template = env.get_template('template.html')
 
 with open("books.json", "r", encoding="utf8") as books_file:
@@ -25,9 +27,9 @@ books_for_rendering = []
 for book in books:
     books_for_rendering.append({'author': book['author'],
                                 'title': book['title'],
-                                'pic': os.path.join('covers', book['img name']),
-                                'path': os.path.join('books', f'{book["filename"]}.txt')})
-books_for_rendering = list(chunked(books_for_rendering, 2))
+                                'pic': os.path.join('../covers', book['img name']),
+                                'path': os.path.join('../books', f'{book["filename"]}.txt')})
+books_for_rendering = list(chunked(books_for_rendering, 10))
 on_reload()
 
 server = Server()
